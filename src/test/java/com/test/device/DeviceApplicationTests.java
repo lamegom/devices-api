36c5d1ce14952181741b14a1fd17c93ca05ba71f
lamegom <lamegom@me.com>
2025-06-01 00:46:58 -0300

    junit error fix

--- src/test/java/com/test/device/DeviceApplicationTests.java
+++ src/test/java/com/test/device/DeviceApplicationTests.java
@@ -2,8 +2,11 @@ package com.test.device;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.test.device.hexagonal.DeviceApplication;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ContextConfiguration(classes = DeviceApplication.class)
 class DeviceApplicationTests {
 
 	@Test
