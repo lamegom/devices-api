59754fe58dad1f80e06a9aa875c41e79ba6b263d
lamegom <lamegom@me.com>
2025-06-02 09:51:57 -0300

    url

--- src/main/java/com/test/device/hexagonal/infrastructure/adapters/input/DeviceRestAdapter.java
+++ src/main/java/com/test/device/hexagonal/infrastructure/adapters/input/DeviceRestAdapter.java
@@ -38,7 +38,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/v1/devices")
+@RequestMapping("/api/v1/devices")
 @RequiredArgsConstructor
 public class DeviceRestAdapter {
     
